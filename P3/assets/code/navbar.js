8dfb29c59d4d4f91b39630dbe058a958ceb17b7c
Imre Angelo <imre@angelo.no>
2019-11-06 15:05:08 +0100

    Navbar Finished
    Might need tweaks

--- P3/assets/code/navbar.js
+++ P3/assets/code/navbar.js
@@ -1,24 +1,38 @@
 // https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
 
-
 (function(public) {
     document.addEventListener('DOMReady', function() {
         toggle = document.getElementById('gn-menustate')
+        header = document.getElementById('gn-header')
         navbar = document.getElementById('navbar')
+        
+        updateStickyOffset()
+    })
+    
+    window.addEventListener('resize', () => updateStickyOffset())
+    window.addEventListener('scroll', () => updateStickyPosition())
+
+//private:
+    function updateStickyOffset() {
+        this.navbar.classList.remove('sticky')
         offset = navbar.offsetTop
 
-        console.log(offset)
-    })
-    
-    window.addEventListener('scroll', function(e) {
-        // console.log(windows.pageYOffset)
+        updateStickyPosition()
+    }
+
+    function updateStickyPosition() {
         if (window.pageYOffset >= offset) {
+            console.log('added sticky')
             this.navbar.classList.add("sticky")
+            return true
         } else {
+            console.log('removed sticky')
             this.navbar.classList.remove("sticky")
+            return false
         }
-    })
+    }
     
+//public:
     public.navbarToggle = function() {
         toggle.checked = !toggle.checked
     }
@@ -26,6 +40,8 @@
     public.navigateTo = function(id) {
         const element = document.getElementById(id)
         const target = element.getBoundingClientRect().top + window.pageYOffset - 44
+
+        public.navbarToggle()
         
         window.scrollTo({
             top: target,
