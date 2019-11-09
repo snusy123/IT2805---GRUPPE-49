6fd9a8de27e7b80c9e914fb586c7b1735e7e103e
Imre Angelo <imre@angelo.no>
2019-11-09 17:07:56 +0100

    Almost

--- P3/assets/code/map.js
+++ P3/assets/code/map.js
@@ -1,70 +1,48 @@
 (function(public) {
-  document.addEventListener("DOMReady", function() {
-    initMapKit();
-    createMap();
-    addMarker();
-  });
+    document.addEventListener("DOMReady", function() {
+        initMapKit();
+        createMap();
+        addMarker();
+        centerMap();
+    });
 
-  function initMapKit() {
-    mapkit.init({
-      authorizationCallback: function(done) {
-        done(
-          "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ikc4UTk3VllBMlIifQ.eyJpc3MiOiJWWDVZTVg5N1g1IiwiaWF0IjoxNTcyODEyNzQ5LCJleHAiOjE2MDc2MzE5NDl9.CE2Yw5stFAJt-9wM6gndbEBT-eXwEjyMEGZ98mn-OwE796OEoTUPut9FHdOJHbgKs9Cgd4ASue_r0baYudBeJw"
+    function initMapKit() {
+        mapkit.init({
+            authorizationCallback: function(done) {
+                done("eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ikc4UTk3VllBMlIifQ.eyJpc3MiOiJWWDVZTVg5N1g1IiwiaWF0IjoxNTcyODEyNzQ5LCJleHAiOjE2MDc2MzE5NDl9.CE2Yw5stFAJt-9wM6gndbEBT-eXwEjyMEGZ98mn-OwE796OEoTUPut9FHdOJHbgKs9Cgd4ASue_r0baYudBeJw");
+            }
+        });
+    }
+
+    function createMap() {
+        public.map = new mapkit.Map("map", {
+            colorScheme: mapkit.Map.ColorSchemes.Dark,
+            isRotationEnabled: true,
+            isZoomEnabled: true,
+            showsZoomControl: true,
+            showsMapTypeControl: false,
+            showsCompass: mapkit.FeatureVisibility.Hidden
+        });
+    }
+
+    function addMarker() {
+        var restaurant = new mapkit.Coordinate(63.431877, 10.2728634);
+
+        var restaurantAnnotation = new mapkit.MarkerAnnotation(restaurant, {
+            color: "maroon",
+            title: "Restaurant",
+            subtitle: "Gamel brynsvei, 7020, Trondheim",
+            selected: "true",
+            glyphText: ":)"
+        });
+
+        public.map.showItems(restaurantAnnotation);
+    }
+
+    function centerMap() {
+        map.region = new mapkit.CoordinateRegion(
+            new mapkit.Coordinate(63.4290637, 10.3130715),
+            new mapkit.CoordinateSpan(0.03, 0.05)
         );
-      }
-    });
-  }
-
-  function createMap() {
-    public.map = new mapkit.Map("map", {
-      colorScheme: mapkit.Map.ColorSchemes.Dark,
-
-      isRotationEnabled: true,
-      isZoomEnabled: true,
-      showsZoomControl: true,
-      showsMapTypeControl: false,
-      showsCompass: mapkit.FeatureVisibility.Hidden
-    });
-  }
-
-  function addMarker() {
-    var MarkerAnnotation = mapkit.MarkerAnnotation;
-    var restaurant = new mapkit.Coordinate(63.431877, 10.2728634);
-
-    var restaurantAnnotation = new MarkerAnnotation(restaurant, {
-      color: "maroon",
-      title: "Restaurant",
-      subtitle: "Gamel brynsvei, 7020, Trondheim",
-      selected: "true",
-      glyphText: ":)"
-    });
-    var center = new mapkit.CoordinateRegion(
-      new mapkit.Coordinate(63.4290637, 10.3130715),
-      new mapkit.CoordinateSpan(0.03, 0.05)
-    );
-    public.map.showItems(restaurantAnnotation);
-    map.region = center;
-  }
+    }
 })(this);
-
-// var clickannotation;
-// // Drop an annotation where a Shift-click is detected:
-// map.element.addEventListener("click", function(event) {
-//   if (!event.shiftKey) {
-//     return;
-//   }
-
-//   if (clickAnnotation) {
-//     map.removeAnnotation(clickAnnotation);
-//   }
-
-//   var coordinate = map.convertPointOnPageToCoordinate(
-//     new DOMPoint(event.pageX, event.pageY)
-//   );
-//   clickAnnotation = new MarkerAnnotation(coordinate, {
-//     title: "Click!",
-//     color: "#c969e0"
-//   });
-//   map.addAnnotation(clickAnnotation);
-// });
-// map.region = center;
